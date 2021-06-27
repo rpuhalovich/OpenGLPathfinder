a695a753cdbc64b335708f6281402b1928c3b67b
rpuhalovich <reanpuhalovich@gmail.com>
2021-06-27 17:56:59 +1000

    Cleanup.

---
+++ src/gfx/Colors.cpp
@@ -0,0 +1,6 @@
+#include "Colors.hpp"
+
+float Colors::normalize(unsigned int value) {
+    if (value > COLOR_RANGE) errorExit("Invalid color range.", EXIT_FAILURE);
+    return value / COLOR_RANGE;
+}
