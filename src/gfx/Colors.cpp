26bc92c630c7ad99e9255aeb9e3b3bea60f51e8e
rpuhalovich <reanpuhalovich@gmail.com>
2021-06-29 00:19:00 +1000

    Fixed extra qualifier error.

--- src/gfx/Colors.cpp
+++ src/gfx/Colors.cpp
@@ -1,7 +1,7 @@
 #include "Colors.hpp"
 
 namespace Colors {
-    float Colors::normalize(unsigned int value) {
+    float normalize(unsigned int value) {
         if (value > COLOR_RANGE) errorExit("Invalid color range.", EXIT_FAILURE);
         return value / COLOR_RANGE;
     }
